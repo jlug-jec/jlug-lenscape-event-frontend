0834e71203a6e88905a58e12fac86863e73afad6
vikram <awesomevikram3@gmail.com>
2024-10-06 22:35:50 +0530

    fixed:build errors
    Signed-off-by: vikram <awesomevikram3@gmail.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,26 +6,16 @@ import {Header} from "@/components/homePageHeader";
 import {products} from "@/lib/constants";
 import { checkServerStatus,handleGoogleAuth } from "../api/checkServer";
 import { Loader } from "@/components/loader";
-import { useSearchParams } from "next/navigation";
 import { toast } from "react-toastify";
 import { Footer } from "@/components/footer";
-import FAQPopup from "@/components/faq";
+
 
 export default function HomePage() {
   const [isLoading, setIsLoading] = useState(true);
   const [isClient, setIsClient] = useState(false);
-  const searchParams=useSearchParams()
-  const error=searchParams.get("error")
 
 
 
-  useEffect(() => {
-    setIsClient(true);
-    if(error=="session_expired"){
-      toast.warn("session expired, Please Login Again")
-      return
-    }
-  }, []);
 
   useEffect(() => {
     checkServerStatus(setIsLoading);
